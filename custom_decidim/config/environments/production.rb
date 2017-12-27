2e9ac58cbf9f7f6d3d44ef03d75fd0bdc951c33a
Ludivine Coipel <coipel.ludivine@gmail.com>
2017-12-27 15:17:44 +0100

    Edit config files + edit override.scss

--- custom_decidim/config/environments/production.rb
+++ custom_decidim/config/environments/production.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => Rails.application.secrets.smtp_address,
     :port           => Rails.application.secrets.smtp_port,
